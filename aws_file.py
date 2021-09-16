2223791b1074165308158ee538b80ef732519623
Enzo MATHE <enzo.mathe+github+test@gitguardian.com>
2021-09-16 10:43:29 +0200

    Add files via upload

---
+++ aws_file.py
@@ -0,0 +1,18 @@
+def aws_upload(data: Dict):
+    database = aws_lib.connect("AKIAF6BAFJKR45SAWSZ5", "hjshnk5ex5u34565AWS654/JKGjhz545d89sjkja")
+    database.push(data)
+
+
+def transform_data(es_data: Dict) -> Dict:
+    es_data = {**data, "origin": "ES"}
+
+
+AIzaSyCpOSpuzaS2VF_fZb9piR7nfDNNeXo7uqM
+AIzaSyCpOSpuzaS2VF_fZb9piR7nfDNNeXo7uqM
+AIzaSyCpOSpuzaS2VF_fZb9piR7nfDNNeXo7uqM
+
+
+AIzaSyCpOSpuzaS2VF_fZb9piR7nfDNNeXo7uqR
+
+AIzaSyCpOSpuzaS2VF_fZb9piR7nfDNNeXo7uqM
+AIzaSyCpOSpuzaS2VF_fZb9piR7nfDNNeXo7uqM
